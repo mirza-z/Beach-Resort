c0cae64f2dcbba690d55afd2a6b0c3a9c8d99652
mirza-z <zaimovic330@gmail.com>
2023-05-08 18:47:02 +0200

    Made a Navbar, HomePage, and a Error page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+// Components 
+import Navbar from './components/Navbar';
+
 // Router Dom
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route exact path='/rooms/' element={<Rooms/>}/>
